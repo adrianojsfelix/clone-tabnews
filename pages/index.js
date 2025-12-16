263e10b0f4a8e12cf9885972828e1e82ae331dd1
Adriano JS Felix <adrianojsfelix@gmail.com>
2025-12-16 18:26:02 +0000

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 function Home() {
-    return <h1>Suzana eu te amo muito! Se você me ama também, dá um sorrisinho :)</h1>
+  return (
+    <h1>Suzana eu te amo muito! Se você me ama também, dá um sorrisinho :)</h1>
+  );
+}
 
+function teste() {
+  console.log("teste");
+}
+
+function teste() {
+  console.log("indentação errada");
 }
 
 export default Home;
